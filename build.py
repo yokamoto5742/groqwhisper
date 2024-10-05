4f27b8c67fc145f71e383775fe94dc0cfb252406
yokamoto5742-h <yokamoto5742@gmail.com>
2024-10-05 14:45:54 +0900

    build.pyの修正

--- build.py
+++ build.py
@@ -1,14 +1,12 @@
 import subprocess
-
+import shutil
 from version_manager import update_version, update_main_py
 
 
 def build_executable():
-    """実行可能ファイルをビルドし、必要なファイルをコピーする"""
     new_version = update_version()
     update_main_py(new_version)
 
-    # PyInstallerを実行
     subprocess.run([
         "pyinstaller",
         "--name=GroqWhisper",
@@ -16,6 +14,10 @@ def build_executable():
         "--icon=assets/GroqWhisper.ico",
         "main.py"
     ])
+
+    # 必要なファイルをdistフォルダにコピー
+    shutil.copy("config.ini", "dist/GroqWhisper")
+    shutil.copy("replacements.txt", "dist/GroqWhisper")
 
     print(f"Executable built successfully. Version: {new_version}")
 
