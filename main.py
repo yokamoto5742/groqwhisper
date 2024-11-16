c932fc374eb5db1412f2d4f69757c18fe3b37c8c
yokamoto5742-h <yokamoto5742@gmail.com>
2024-11-16 15:50:19 +0900

    VERSION = "1.0.8" LAST_UPDATED = "2024/11/16"

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from app_window import AudioRecorderGUI
 from service_text_processing import load_replacements
 from service_transcription import setup_groq_client
 
-VERSION = "1.0.7"
-LAST_UPDATED = "2024/11/12"
+VERSION = "1.0.8"
+LAST_UPDATED = "2024/11/16"
 
 
 def main():
