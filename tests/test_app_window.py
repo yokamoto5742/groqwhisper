8d5defdb2e1a3e4aec0d62991dbc6555fb98e81d
yokamoto5742-h <yokamoto5742@gmail.com>
2025-02-24 14:58:14 +0900

    コンフィグの修正

--- tests/test_app_window.py
+++ tests/test_app_window.py
@@ -74,6 +74,28 @@ def voice_input_manager(mock_tk_root, config, mock_recorder, mock_client, mock_r
         return manager
 
 
+def restore_config(config, original_config):
+    """configを元の状態に復元するヘルパーメソッド"""
+    for section in config.sections():
+        config.remove_section(section)
+
+    for section in original_config.sections():
+        if not config.has_section(section):
+            config.add_section(section)
+        for key, value in original_config[section].items():
+            config[section][key] = value
+
+
+def restore_config_and_controller(config, original_config, voice_input_manager):
+    """configとコントローラーの状態を復元するヘルパーメソッド"""
+    restore_config(config, original_config)
+
+    use_punctuation = original_config['WHISPER'].getboolean('USE_PUNCTUATION', False)
+    use_comma = original_config['WHISPER'].getboolean('USE_COMMA', False)
+    voice_input_manager.recording_controller.use_punctuation = use_punctuation
+    voice_input_manager.recording_controller.use_comma = use_comma
+
+
 class TestVoiceInputManager:
     def test_init_starts_minimized(self, voice_input_manager, mock_tk_root, config):
         """START_MINIMIZEDがTrueの場合、ウィンドウが最小化されることを確認"""
@@ -82,7 +104,7 @@ class TestVoiceInputManager:
             mock_tk_root.iconify.assert_called_once()
         finally:
             # configを元の状態に復元
-            self._restore_config(config, original_config)
+            restore_config(config, original_config)
 
     def test_toggle_punctuation(self, voice_input_manager, config):
         """句読点の切り替えが正しく動作することを確認"""
@@ -110,7 +132,7 @@ class TestVoiceInputManager:
             voice_input_manager.ui_components.update_punctuation_button.assert_called_with(True)
         finally:
             # configとコントローラーの状態を復元
-            self._restore_config_and_controller(config, original_config, voice_input_manager)
+            restore_config_and_controller(config, original_config, voice_input_manager)
 
     def test_close_application(self, voice_input_manager, mock_tk_root, config):
         """アプリケーションの終了処理が正しく実行されることを確認"""
@@ -124,7 +146,7 @@ class TestVoiceInputManager:
             voice_input_manager.notification_manager.cleanup.assert_called_once()
             mock_tk_root.quit.assert_called_once()
         finally:
-            self._restore_config(config, original_config)
+            restore_config(config, original_config)
 
     def test_toggle_recording(self, voice_input_manager, config):
         """録音の切り替えが正しく動作することを確認"""
@@ -133,7 +155,7 @@ class TestVoiceInputManager:
             voice_input_manager.toggle_recording()
             voice_input_manager.recording_controller.toggle_recording.assert_called_once()
         finally:
-            self._restore_config(config, original_config)
+            restore_config(config, original_config)
 
     @pytest.mark.parametrize("start_minimized", [True, False])
     def test_init_window_state(self, mock_tk_root, config, mock_recorder,
@@ -156,27 +178,4 @@ class TestVoiceInputManager:
                 else:
                     mock_tk_root.iconify.assert_not_called()
         finally:
-            self._restore_config(config, original_config)
-
-    def _restore_config(self, config, original_config):
-        """configを元の状態に復元するヘルパーメソッド"""
-        # 現在のconfigの全セクションをクリア
-        for section in config.sections():
-            config.remove_section(section)
-
-        # original_configから全ての設定を復元
-        for section in original_config.sections():
-            if not config.has_section(section):
-                config.add_section(section)
-            for key, value in original_config[section].items():
-                config[section][key] = value
-
-    def _restore_config_and_controller(self, config, original_config, voice_input_manager):
-        """configとコントローラーの状態を復元するヘルパーメソッド"""
-        self._restore_config(config, original_config)
-
-        # コントローラーの状態も元に戻す
-        use_punctuation = original_config['WHISPER'].getboolean('USE_PUNCTUATION', False)
-        use_comma = original_config['WHISPER'].getboolean('USE_COMMA', False)
-        voice_input_manager.recording_controller.use_punctuation = use_punctuation
-        voice_input_manager.recording_controller.use_comma = use_comma
+            restore_config(config, original_config)
